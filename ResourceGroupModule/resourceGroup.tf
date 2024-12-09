65e5c7ee421f565c3511aa2bc58dfff1dc1eb20d
Steve Keller <stkeller@microsoft.com>
2024-12-09 11:40:04 -0700

    added version tag

--- ResourceGroupModule/resourceGroup.tf
+++ ResourceGroupModule/resourceGroup.tf
@@ -3,6 +3,7 @@ resource "azurerm_resource_group" "thisresourcegroup" {
   location = var.resource_group_location_module
   tags = {
     codesource = "module from Terraform-Modules repo."
+    version = "1.1"
   }
 }
 
